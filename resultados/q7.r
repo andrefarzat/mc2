a3de27c7f6c5269e7c521099baf9a80c3b4c135a
Andre Farzat <andrefarzat@gmail.com>
2017-12-06 14:08:38 -0200

    borders

--- resultados/q7.r
+++ resultados/q7.r
@@ -5,7 +5,6 @@
 library(ggplot2)
 library(cowplot)
 
-options(tz="Europe/Berlin")
 setwd("/Users/andrefarzat/Documents/mc2/dados")
 
 NSGA        <- cbind(read.table(file="data_t7_nsga_150k_c50_2x_error1_frontier_obj.txt", header=TRUE),  list(metodo = "NSGA"));
@@ -22,53 +21,26 @@ dadosPARM   <- subset(dados, inst == "PARM");
 shapes <- c(1, 1, 1, 2, 3)
 colors <- c("light gray", "dark gray", "black", "black", "black")
 
-plotAcadMkspanCusto  <- ggplot(dadosACAD, aes(x=mks, y=cst/1000, color=metodo, shape=metodo)) +
+plotAcadMkspanCusto <- ggplot(dadosACAD, aes(x = mks, y = cst / 1000, color = metodo, shape = metodo)) +
                         geom_point() +
-                        scale_shape_manual(values=shapes) +
-                        scale_color_manual(values=colors) +
+                        scale_shape_manual(values = shapes) +
+                        scale_color_manual(values = colors) +
                         labs(y = "Cost(1000$)", x = "Makespan (days)") +
                         ggtitle("ACAD") +
-                        theme(plot.title = element_text(face="bold", hjust=0.98,margin = margin(b = -20)), legend.position="none", plot.margin=grid::unit(c(0.3,0.1,0.1,0.1), "in"))
+                        panel_border(colour = "black",size = 1) +
+                        theme(
+                        panel.background = element_rect(fill = "white", colour = "grey50"),
+                        plot.title = element_text(face = "bold", hjust = 0.98, margin = margin(b = -20)),
+                        legend.position = "none",
+                        plot.margin = grid::unit(c(0.3, 0.1, 0.1, 0.1), "in")
+                        )
 
-plotAcadOvtMkspan    <- ggplot(dadosACAD, aes(x=noh*8, y=mks, color=metodo, shape=metodo)) +
-                        geom_point() +
-                        scale_shape_manual(values=shapes) +
-                        scale_color_manual(values=colors) +
-                        labs(y = "Makespan (days)", x = "Overtime (hours)") +
-                        ggtitle("ACAD") +
-                        theme(plot.title = element_text(face="bold", hjust=0.98,margin = margin(b = -20)), legend.position="none", plot.margin=grid::unit(c(0.3,0.1,0.1,0.1), "in"))
+plotAcadOvtMkspan <- ggplot(dadosACAD, aes(x = noh*8, y = mks, color = metodo, shape = metodo)) + geom_point() + scale_shape_manual(values = shapes) + scale_color_manual(values = colors) + labs(y = "Makespan (days)", x = "Overtime (hours)") + ggtitle("ACAD") + theme(panel.border = element_rect(colour = "black", fill = NA, size = 5), plot.title = element_text(face = "bold", hjust = 0.98, margin = margin(b = -20)), legend.position = "none", plot.margin = grid::unit(c(0.3, 0.1, 0.1, 0.1), "in"))
+plotAcadOvtCusto <- ggplot(dadosACAD,aes(x=noh*8,y=cst/1000,color=metodo,shape=metodo)) + geom_point() + scale_shape_manual(values=shapes) + scale_color_manual(values=colors) + labs(y = "Cost(1000$)", x = "Overtime (hours)") + ggtitle("ACAD")+ theme(plot.title = element_text(face="bold", hjust=0.98,margin = margin(b = -20)), legend.position="none", plot.margin=grid::unit(c(0.3,0.1,0.1,0.1), "in"))
 
-plotAcadOvtCusto     <- ggplot(dadosACAD,aes(x=noh*8, y=cst/1000, color=metodo, shape=metodo)) +
-                        geom_point() +
-                        scale_shape_manual(values=shapes) +
-                        scale_color_manual(values=colors) +
-                        labs(y = "Cost(1000$)", x = "Overtime (hours)") +
-                        ggtitle("ACAD") +
-                        theme(plot.title = element_text(face="bold", hjust=0.98,margin = margin(b = -20)), legend.position="none", plot.margin=grid::unit(c(0.3,0.1,0.1,0.1), "in"))
-
-plotParmMkspanCusto  <- ggplot(dadosPARM,aes(x=mks,y=cst/1000,color=metodo,shape=metodo)) +
-                        geom_point() +
-                        scale_shape_manual(values=shapes) +
-                        scale_color_manual(values=colors) +
-                        labs(y = "Cost(1000$)", x = "Makespan (days)") +
-                        ggtitle("PARM")+
-                        theme(plot.title = element_text(face="bold", hjust=0.98,margin = margin(b = -20)), legend.position="none", plot.margin=grid::unit(c(0.3,0.1,0.1,0.1), "in"))
-
-plotParmOvtMkspan    <- ggplot(dadosPARM,aes(x=noh*8, y=mks, color=metodo, shape=metodo)) +
-                        geom_point() +
-                        scale_shape_manual(values=shapes) +
-                        scale_color_manual(values=colors) +
-                        labs(y = "Makespan (days)", x = "Overtime (hours)") +
-                        ggtitle("PARM")+
-                        theme(plot.title = element_text(face="bold", hjust=0.98,margin = margin(b = -20)), legend.position="none", plot.margin=grid::unit(c(0.3,0.1,0.1,0.1), "in"))
-
-plotParmOvtCusto     <- ggplot(dadosPARM,aes(x=noh*8, y=cst/1000, color=metodo, shape=metodo)) +
-                        geom_point() +
-                        scale_shape_manual(values=shapes) +
-                        scale_color_manual(values=colors) +
-                        labs(y = "Cost(1000$)", x = "Overtime (hours)") +
-                        ggtitle("PARM")+
-                        theme(plot.title = element_text(face="bold", hjust=0.98,margin = margin(b = -20)), legend.position="none", plot.margin=grid::unit(c(0.3,0.1,0.1,0.1), "in"))
+plotParmMkspanCusto <- ggplot(dadosPARM,aes(x=mks,y=cst/1000,color=metodo,shape=metodo)) + geom_point() + scale_shape_manual(values=shapes) + scale_color_manual(values=colors) + labs(y = "Cost(1000$)", x = "Makespan (days)") + ggtitle("PARM")+ theme(plot.title = element_text(face="bold", hjust=0.98,margin = margin(b = -20)), legend.position="none", plot.margin=grid::unit(c(0.3,0.1,0.1,0.1), "in"))
+plotParmOvtMkspan <- ggplot(dadosPARM,aes(x=noh*8,y=mks,color=metodo,shape=metodo)) + geom_point() + scale_shape_manual(values=shapes) + scale_color_manual(values=colors) + labs(y = "Makespan (days)", x = "Overtime (hours)") + ggtitle("PARM")+ theme(plot.title = element_text(face="bold", hjust=0.98,margin = margin(b = -20)), legend.position="none", plot.margin=grid::unit(c(0.3,0.1,0.1,0.1), "in"))
+plotParmOvtCusto <- ggplot(dadosPARM,aes(x=noh*8,y=cst/1000,color=metodo,shape=metodo)) + geom_point() + scale_shape_manual(values=shapes) + scale_color_manual(values=colors) + labs(y = "Cost(1000$)", x = "Overtime (hours)") + ggtitle("PARM")+ theme(plot.title = element_text(face="bold", hjust=0.98,margin = margin(b = -20)), legend.position="none", plot.margin=grid::unit(c(0.3,0.1,0.1,0.1), "in"))
 
 final <- plot_grid(plotAcadMkspanCusto, plotAcadOvtMkspan, plotAcadOvtCusto, plotParmMkspanCusto, plotParmOvtMkspan, plotParmOvtCusto, nrow=2, ncol=3)
 
